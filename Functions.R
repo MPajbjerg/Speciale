ff5f9835f9b79e76852aa60ca631b5066a4dbdc8
Mathias Pajbjerg <mpj@appension.dk>
2025-04-24 16:21:34 +0200

    Added Euler scheme, numerical integral as well as a test of the setup with the Dahl & Møller setup.

---
+++ Functions.R
@@ -0,0 +1,24 @@
+#Implementing an Euler-Maruyama scheme
+eulerMaruyama <- function(stepSize,drift,diffusion,initialValue,numberOfSteps,time){
+  #Initializing vector to store values
+  output <- matrix(nrow=numberOfSteps+1, ncol= 2)
+  output[1,1] <- time
+  output[1,2] <- initialValue
+  #Simulating brownian motion increments
+  deltaW <- rnorm(numberOfSteps,0,sqrt(stepSize))
+  for (i in 1:(numberOfSteps)){
+    #calculating value
+    output[i+1,2] <- output[i,2]+drift(output[i,2],output[i,1])*stepSize +
+      diffusion(output[i,2],output[i,1])*deltaW[i]
+    #increasing time for next step
+    output[i+1,1] <- output[i,1]+stepSize
+  }
+  return(output)
+}
+
+#Implementing numerical integration by the trapezoidal rule
+#Taken from LIV2 Week_5 file "ODEandInt"
+numIntegrate <- function(lowerlim, upperlim, integrand, stepSize){
+  yval <- integrand(seq(lowerlim,upperlim,stepSize))
+  return(sum(yval[-1]+yval[-(length(yval))])*stepSize/2)
+}
--- Main.R
+++ Main.R
@@ -1,3 +1,72 @@
 #Sourcing functions
 source("Functions.R")
 
+#testing with gompertz-makeham from Dahl & Møller 2006.
+
+{
+  startAge <- 10
+  alpha <- 0.000134
+  beta <- 0.0000353
+  c <- 1.1020
+  
+  gompertzMu <- function(t){
+    alpha + beta*c^(startAge+t)
+  }
+  
+  gompertzMuDerivative <- function(t){
+    beta*log(c)*c^(startAge+t)
+  }
+  
+  #Defining parameters
+  deltaTilde <- 0.2 
+  gammaTilde <- 0.008
+  sigmaTilde <- 0.02
+
+  delta <- deltaTilde
+  gamma <- function(t){
+    g <- deltaTilde*exp(-gammaTilde*t)
+    return(g)
+  }
+  
+  sigma <- sigmaTilde
+  
+  deltaMu <- function(t){
+    delta - (gompertzMuDerivative(t)/gompertzMu(t)) 
+  }
+  
+  gammaMu <- function(t){
+    gamma(t)*gompertzMu(t)
+  }
+  
+  sigmaMu <- function(t){
+    sigma*sqrt(gompertzMu(t))
+  }
+  
+  drift <- function(mu,t){
+    gammaMu(t)-deltaMu(t)*mu
+  }
+  
+  diffusion <- function(mu,t){
+    sigmaMu(t)*sqrt(mu)
+  }
+  
+  initial <- gompertzMu(0)
+  
+  
+  #Testing how stochastic mortality develops.
+  testStocMu <- eulerMaruyama(0.1,drift,diffusion,initial,1000,0)
+  
+  plot(testStocMu[,1]+startAge,testStocMu[,2],type = "l",
+       xlab = "age in years", ylab = "intensity", col = "black", lwd = 2)
+  grid()
+  
+  testStocMu2 <- eulerMaruyama(0.1,drift,diffusion,initial,1000,0)
+  
+  lines(testStocMu2[,1]+startAge,testStocMu2[,2],type = "l", col = "red",
+        lwd = 2)
+  
+  testStocMu3 <- eulerMaruyama(0.1,drift,diffusion,initial,1000,0)
+  
+  lines(testStocMu3[,1]+startAge,testStocMu3[,2],type = "l", col = "blue",
+        lwd = 2)
+}
